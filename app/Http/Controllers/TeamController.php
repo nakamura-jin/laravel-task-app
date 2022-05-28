8f5728b2ee000944d549dd8b9554c086e5148bf3
中村　迅 <shu.kyu0224@icloud.com>
2022-05-28 19:12:49 +0900

    Team Controller CRUD作成

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -3,8 +3,71 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\TeamRequest;
+use App\Models\Team;
 
 class TeamController extends Controller
 {
-    //
+    public function index()
+    {
+        $teams = Team::all();
+
+        return response()->json(['teams' => $teams], 200);
+    }
+
+    public function create(TeamRequest $request)
+    {
+        $input = $request->validated();
+
+        $team = Team::create([
+            'name' => $input['name'],
+            'user_id' => $input['user_id'],
+        ]);
+
+        if(!$team) {
+            return response()->json(['message' => 'error'], 404);
+        }
+
+        return response()->json(['team' => $team], 201);
+    }
+
+    public function show(Request $request)
+    {
+        $team = Team::find($request->id);
+
+        if(!$team) {
+            return response()->json(['message' => 'Team not found'], 404);
+        }
+
+        return response()->json(['team' => $team], 200);
+    }
+
+    public function edit(Request $request)
+    {
+        $data = [
+            'name' => $request->name,
+            'user_id' => $request->user_id,
+        ];
+
+        $update = Team::where('id', $request->id)->update($data);
+
+        if(!$update) {
+            return response()->json(['message' => 'Could not update.'], 404);
+        }
+
+        return response()->json(['message' => 'Successfully'], 200);
+
+    }
+
+    public function delete(Request $request)
+    {
+        $team = Team::where('id', $request->id)->delete();
+
+        if(!$team) {
+            return response()->json(['message' => 'Could not delete.'], 404);
+        }
+
+        return response()->json(['message' => 'Successfully'], 200);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TeamController;
 
 
 Route::group(['middleware' => 'api', 'prefix' => 'v1'], function() {
@@ -10,4 +11,10 @@ Route::group(['middleware' => 'api', 'prefix' => 'v1'], function() {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
+
+    Route::get('team', [TeamController::class, 'index']);
+    Route::post('team', [TeamController::class, 'create']);
+    Route::get('team/{id}', [TeamController::class, 'show']);
+    Route::put('team/{id}', [TeamController::class, 'edit']);
+    Route::delete('team/{id}', [TeamController::class, 'delete']);
 });
